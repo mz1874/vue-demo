2ac20b84b1948a65602d2f3f7e98755c90443ae4
wangchong <mz1874@aliyun.com>
2020-07-30 17:34:47 +0800

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    我是新导入的组件{{msg}}
+    <el-radio v-model="radio" label="1">备选项1</el-radio>
+    <el-radio v-model="radio" label="2">备选项2</el-radio>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Hello",
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        radio: "2",
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
